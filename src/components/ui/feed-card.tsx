df5e78b5204e740a531337a50926d8d9407386dd
도니형 <dohun3182@gmail.com>
2026-02-17 01:10:13 +0900

    fix: 간격 수정

--- src/components/ui/feed-card.tsx
+++ src/components/ui/feed-card.tsx
@@ -195,8 +195,8 @@ function FeedCard({
     >
       <Stack gap={16}>
         {/* Header */}
-        <Group align="center" justify="between">
-          {(profileImage || username || category || timeAgo) && (
+        {(profileImage || username || category || timeAgo) && (
+          <Group align="center" justify="between">
             <Group gap={10} align="center">
               {profileImage && (
                 <div className="size-[32px] shrink-0 overflow-hidden rounded-full bg-gray-300">
@@ -242,8 +242,8 @@ function FeedCard({
                 )}
               </Stack>
             </Group>
-          )}
-        </Group>
+          </Group>
+        )}
 
         <Stack gap={12} className="rounded-2xl bg-gray-100 px-4 py-3.5">
           {content && (
