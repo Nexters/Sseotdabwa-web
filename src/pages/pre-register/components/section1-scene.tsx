dcff40cdc8f8e6c9bdb1f1ed9929ca81a432977a
도니형 <dohun3182@gmail.com>
2026-02-21 20:31:41 +0900

    feat: section 1 수정

--- src/pages/pre-register/components/section1-scene.tsx
+++ src/pages/pre-register/components/section1-scene.tsx
@@ -36,15 +36,12 @@ function TobongLottie({ style }: { style?: React.CSSProperties }) {
     loop: true,
     autoplay: true,
     style: { width: "100%", height: "100%", display: "block" },
-  })
+  });
   return (
-    <div
-      className="absolute"
-      style={style}
-    >
+    <div className="absolute" style={style}>
       {View}
     </div>
-  )
+  );
 }
 
 interface Section1SceneProps {
@@ -81,13 +78,11 @@ function Section1Scene({
   const charHeight = lerp(charStartHeight, charEndHeight, ep);
 
   const logoTop = containerHeight * 0.04;
-  const bubble1Top = containerWidth > 375 ? 52 : 60;
   const bubble2Bottom = charEndBottom + charEndHeight + 26;
   const hintBottom = containerHeight * 0.06;
 
   const titleOpacity = mapRange(progress, 0, 0.35, 1, 0);
   const hintOpacity = mapRange(progress, 0, 0.25, 1, 0);
-  const bubble1Opacity = mapRange(progress, 0.35, 0.55, 1, 0);
   const bubble2Opacity = mapRange(progress, 0.7, 0.9, 0, 1);
 
   const holdDistance = containerHeight;
@@ -98,10 +93,10 @@ function Section1Scene({
   return (
     <div className="sticky top-0 h-screen" style={{ overflow: "clip" }}>
       <div
-        className="absolute left-5"
+        className="absolute left-[24px]"
         style={{ top: logoTop, opacity: titleOpacity }}
       >
-        <Logo />
+        <Logo width={100} height={24} />
       </div>
 
       <TobongLottie
@@ -113,21 +108,6 @@ function Section1Scene({
           transform: `translateX(${charTranslateX}%)`,
         }}
       />
-
-      <SpeechBubble
-        centerArrow
-        animateOn="visible"
-        isVisible={bubble1Opacity > 0.01}
-        animateDelayMs={300}
-        style={{
-          position: "absolute",
-          top: bubble1Top,
-          right: 40,
-          opacity: bubble1Opacity,
-        }}
-      >
-        궁금하면 스크롤해줘!
-      </SpeechBubble>
 
       <SpeechBubble
         centerArrow
@@ -147,7 +127,7 @@ function Section1Scene({
       </SpeechBubble>
 
       <div
-        className="absolute left-1/2 flex -translate-x-1/2 animate-bounce flex-col items-center gap-[4px] [animation-duration:1.8s]"
+        className="absolute left-1/2 flex -translate-x-1/2 animate-bounce flex-col items-center gap-[10px] [animation-duration:1.8s]"
         style={{ bottom: hintBottom, opacity: hintOpacity }}
       >
         <Typography variant="t1-bold" className="text-gray-0">
