49b54468aebe36adda9e2dddd5a7312f3cebae98
도니형 <dohun3182@gmail.com>
2026-02-21 19:58:22 +0900

    chore: gogo

--- src/components/ui/fab.tsx
+++ src/components/ui/fab.tsx
@@ -7,11 +7,9 @@ import { usePreRegister } from "@/pages/pre-register/components/pre-register-pro
 interface FABProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 function FAB({ className, ...props }: FABProps) {
-  const [isOpen, setIsOpen] = React.useState(false);
   const { open: openPreRegister } = usePreRegister();
 
   const handleClick = () => {
-    setIsOpen(true);
     openPreRegister();
   };
 
@@ -24,7 +22,7 @@ function FAB({ className, ...props }: FABProps) {
         "size-[60px] rounded-full",
         "transition-all duration-300 ease-in-out",
         "shadow-[0_4px_30px_rgba(0,0,0,0.2)]",
-        isOpen ? "bg-white" : "bg-gray-800",
+        "bg-gray-800",
         className,
       )}
       onClick={handleClick}
@@ -35,7 +33,7 @@ function FAB({ className, ...props }: FABProps) {
         size={24}
         className={cn(
           "transition-transform duration-300 ease-in-out",
-          isOpen ? "rotate-45 text-gray-800" : "rotate-0 text-white",
+          "rotate-0 text-white",
         )}
       />
     </button>
